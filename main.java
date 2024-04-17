91e231d38ef45a39367470d44a11a5c7a481334a
MIGHTY1o1 <shubhagarwalcse@gmail.com>
2024-04-17 22:27:59 +0530

    add main.java file for programs

---
+++ main.java
@@ -0,0 +1,79 @@
+
+
+public class main {
+    
+    public static void main(String[] args) {
+        // int x =10;
+        // int y =0;
+        // do{ 
+        // System.out.println(x+"");
+        // x++;
+        // y++;
+        // }
+        // while( y<10);
+
+
+        //table of 10 
+        //how can i break at 12 
+        // int x =2;
+        // int ans=0;
+        // for(int i =0; i<=10; i++){
+        //     ans = x*i;
+        //     if(ans==12){
+        //         System.out.println(x+"*"+i+"="+ans);
+        //         break;
+        //     }   
+        //     System.out.println(x+"*"+i+"="+ans);
+        // }
+        // System.out.println("we are outside the loop");
+
+        //insertion 
+        //deletion 
+        //traversal
+            //forward
+            //backward
+        
+            // int arr [] = {1,2};
+            // int len = arr.length;
+            // System.out.println(len);
+
+            // int arr2[]={1,2};
+            // arr2[0]= 1;
+            // arr2[0]= 2;
+            // arr2[0]= 3;
+            // arr2[0]= 4;
+            // arr2[1]= 1;
+            // System.out.println(arr2[1]);
+            // int[] arr = new int[10];
+            // for(int start =0; start<arr.length; start++){
+            //         System.out.print(" "+arr[start]);
+            // }
+
+            int arr2[]={1,2,3,4,5,6,7};
+                    //  0,1,2,3,4,5,6
+            //starting = arr.length - 1
+            //ending = 0
+  
+            for(int start = arr2.length-1; start>=0; start--){
+                System.out.print(" "+arr2[start]);
+             }
+    
+             //10,20,30,40,50,60,70
+
+            // int arr2[]={1,2,3,4,5,6,7};
+            //wap to find if k exist or not if yes print true if not print false 
+            //here k is any element given to user
+            //wap a program to print 10, 20 ,30 
+
+/*
+ * Array data structure 
+ * Array memory representation 
+ * Array traversal 
+ * Array insertion basic 
+ * Array length 
+ */
+
+            
+  
+    }
+}
